8302dcd8ec9ae25b6e909b35ab1d684cb8385ad5
Dany Lopes Gonçalves <LopDa093@school.lu>
2024-05-21 23:09:45 +0200

    added User Logic and removed redundant migration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,62 +4,70 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class UserController
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+        return view('users.register');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $formFields = $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => [
+                'required', Password::min(8)
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(2),
+                'confirmed'
+            ],
+        ]);
+
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        $user = User::create($formFields);
+
+        auth()->login($user);
+
+        return redirect('/boards');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
+    public function logout(Request $request)
     {
-        //
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'User logged out Successfully!');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
+    public function login()
     {
-        //
+        return view('users.login');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
+    public function authenticate(Request $request)
     {
-        //
-    }
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
+        
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/boards');
+        }
+
+        return back()->withErrors(['login' => 'Invalid Credentials']);
     }
 }
